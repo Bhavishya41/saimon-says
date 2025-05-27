36e56d10aba284774247f0895c0e336ed9851203
bhavishya <160098017+Bhavishya41@users.noreply.github.com>
2025-05-28 01:03:55 +0530

    Add files via upload

---
+++ simon.js
@@ -0,0 +1,63 @@
+let colours = ["red","yellow","green","blue"];
+let sequence = [];
+let userSequence = [];
+let btn;
+let level = 0;
+let start = false;
+let levelNo = document.querySelector("h3");
+let highest = 0;
+let high = document.querySelector("h2");
+
+document.addEventListener("keypress", function (){
+    if(start === false){
+        console.log("game started");
+        start = true;
+        levelup();
+    }
+})
+
+function flashBtn(btn) {
+    btn.classList.add("flash");
+    setTimeout(function () {
+        btn.classList.remove("flash");
+    },250);
+}
+
+function levelup(){
+    userSequence = [];
+    level ++;
+    levelNo.innerText = `level ${level}`;
+    let btnIdx = Math.floor(Math.random()*4);
+    let colour = colours[btnIdx];
+    sequence.push(colour);
+    btn = document.querySelector("." + colour);
+    flashBtn(btn);
+}
+
+function btnPress(){
+    let button = this;
+    flashBtn(this);
+    let colour = button.getAttribute("id");
+    console.log(colour);
+    userSequence.push(colour);
+    match(userSequence.length-1);
+}
+
+function match(idx){
+    if(sequence[idx] === userSequence[idx]){
+        if(userSequence.length === sequence.length){
+            setTimeout(levelup, 1000);
+        }
+    }else{
+        levelNo.innerText = `Game over! press any key to restart`;
+        highest = Math.max(highest,level);
+        level = 0;
+        start = false; 
+        high.innerHTML = `highest score: ${highest}`;
+    }
+}
+
+let allBtns = document.querySelectorAll("button");
+for (let btn of allBtns) {
+    btn.addEventListener("click", btnPress);
+}
